21d94234c23666dda92defecbd3e0c7876be80c5
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-10-07 23:03:18 +0300

    added first task to TaskSet3

---
+++ Practical_3/TasksSet3/console_utils.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <windows.h> // Для функцій SetConsoleCP та SetConsoleOutputCP
+
+// Функція для встановлення кодування CP1251 (кирилиця)
+void setCyrillicEncoding();
+
+// Функція для встановлення кодування UTF-8
+void setUtf8Encoding();
