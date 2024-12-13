7f244b15f3ce14e44b734424666358ec172971ea
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-12-13 20:29:59 +0200

    додано Приклад 1. Практичне заняття 13.

--- Practical_13/main.cpp
+++ Practical_13/main.cpp
@@ -8,11 +8,33 @@ void setUtf8Encoding()
     SetConsoleOutputCP(65001);
 }
 
+// Приклад 1.
+const int n = 10; // Розмір масиву
+
+// Функція для обчислення суми від'ємних елементів масиву
+void sumNegativeElements()
+{
+    int a[n];
+    int suma = 0; // Змінна для зберігання суми
+
+    cout << "Введіть масив А (10 елементів):\n";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i]; // Введення елементів масиву
+        if (a[i] < 0) // Перевірка на від'ємність
+        {
+            suma += a[i]; // Додавання до суми
+        }
+    }
+
+    cout << "Сума від'ємних елементів = " << suma << endl;
+}
+
 int main()
 {
     // setlocale(LC_ALL, "uk_UA"); // for my Linux system.
     setUtf8Encoding(); // for my Windows system.
 
-    std::cout << "Привіт, Світе!" << std::endl;
+    sumNegativeElements();
     return 0;
 }
