1c02e890eb3ee6175764dbcbb3eae402a8a827d7
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-12-13 20:12:26 +0200

    Ukrainian language add-on for Windows/Linux system

--- Practical_12/main.cpp
+++ Practical_12/main.cpp
@@ -154,15 +154,7 @@ int main()
 
     // executeSeriesCalculation();
     // executeSeriesCalculation2();
-    // executeArctanComparison();
-
-    int i=5;
-
-    do {cout<< (++i)++ <<" "; }
-
-    while( i>=5 && i<8 );
-
-
+    executeArctanComparison();
 
     return 0;
 }
--- Practical_13/main.cpp
+++ Practical_13/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <windows.h>
 
-int main() {
-    setlocale(LC_ALL, "uk_UA");
+using namespace std;
+void setUtf8Encoding()
+{
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+}
+
+int main()
+{
+    // setlocale(LC_ALL, "uk_UA"); // for my Linux system.
+    setUtf8Encoding(); // for my Windows system.
 
     std::cout << "Привіт, Світе!" << std::endl;
     return 0;
