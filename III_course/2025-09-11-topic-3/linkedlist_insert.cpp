07a171b10009425a7a1565852e34f4edd251373e
yourhostel <yourhostel.ua@gmail.com>
2025-09-11 01:48:18 +0300

    init of the project topic 3

---
+++ III_course/2025-09-11-topic-3/linkedlist_insert.cpp
@@ -0,0 +1,10 @@
+// III_course/2025-09-11-topic-3/linkedlist_insert.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ III_course/2025-09-11-topic-3/vector_map_search.cpp
@@ -0,0 +1,10 @@
+// III_course/2025-09-11-topic-3/vector_map_search.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
