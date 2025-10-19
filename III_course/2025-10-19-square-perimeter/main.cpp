c1a239d43225254f7a689527f32c9d97e61f7d38
yourhostel <yourhostel.ua@gmail.com>
2025-10-19 20:27:06 +0300

    Init job: squarePerimeter, ringArea, rightTrianglePerimeter, twoDigitAnalysis

---
+++ III_course/2025-10-19-square-perimeter/main.cpp
@@ -0,0 +1,14 @@
+// III_course/2025-10-19-square-perimeter/main.cpp
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main()
+{
+    // TODO:     squarePerimeter();
+    // TODO:     ringArea();
+    // TODO:     rightTrianglePerimeter();
+    // TODO:     twoDigitAnalysis();
+    throw std::logic_error("Not implemented yet");
+}
