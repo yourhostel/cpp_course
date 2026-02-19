6650bbe950c748b440e1b2da8f9fe59603b09fe2
yourhostel <yourhostel.ua@gmail.com>
2026-02-19 03:43:49 +0200

    Додано коментари до 3 тема поліморфізм

--- III_course/2026-02-18-polymorphism2/power/FastPowerStrategy.h
+++ III_course/2026-02-18-polymorphism2/power/FastPowerStrategy.h
@@ -11,8 +11,30 @@
 class FastPowerStrategy : public PowerStrategy
 {
 public:
+    /*
+    Ітеративне бінарне піднесення до степеня.
+
+    Використовує двійкове розкладання показника.
+    Кожної ітерації показник ділиться на 2,
+    основа зводиться в квадрат.
+    Часова складність O(log |p|).
+    */
     [[nodiscard]] double power(double n, int p) const override;
+
+    /*
+    Обчислює степінь через рекурсивний виклик
+    з показником absP / 2.
+    Якщо показник парний повертає half * half.
+    Якщо непарний повертає base * half * half.
+    Глибина рекурсії O(log |p|).
+    */
     [[nodiscard]] double power_recursive_core(double base, int absP) const override;
+
+    /*
+    Рекурсивне бінарне піднесення до степеня
+    з попередньою валідацією та нормалізацією знака
+    делегує обчислення в power_recursive_core.
+    */
     [[nodiscard]] double power_recursive(double n, int p) const override;
 };
 
